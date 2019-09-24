e2db525a8200c15e8ed3a0b07bd009533a09bee6
Stavros Filosidis <stavrosfil@gmail.com>
2019-09-24 23:41:58 +0300

    Code and prints cleanup

--- gauss.py
+++ gauss.py
@@ -4,19 +4,9 @@ import numpy as np
 from scipy.linalg import solve
 import matplotlib.pyplot as plt
 
-# VecStart_y = [2, 2, 5, 5]
-# VecStart_x = [0, 1, 3, 5]
-# VecStart_z = [0, 1, 1, 5]
-# VecEnd_x = [1, 2, -1, 6]
-# VecEnd_y = [3, 1, -2, 7]
-# VecEnd_z = [1, 0, 4, 9]
-
-vec_y = []
 vec_x = []
+vec_y = []
 vec_z = []
-# VecEnd_x = []
-# VecEnd_y = []
-# VecEnd_z = []
 
 def gauss(A, b, x, n):
 
@@ -25,23 +15,25 @@ def gauss(A, b, x, n):
     for i in range(n):
         x = np.dot(np.linalg.inv(L), b - np.dot(U, x))
         # print(str(i).zfill(3))
-        print(x)
+        # print(x)
         vec_x.append(x[0])
         vec_y.append(x[1])
         vec_z.append(x[2])
     return x
 
 
-A = np.array([[4.0, -2.0, 1.0], [1.0, -3.0, 2.0], [-1.0, 2.0, 6.0]])
+A = np.array([[4.0, -2.0, 1.0],
+              [1.0, -3.0, 2.0],
+              [-1.0, 2.0, 6.0]])
+
 b = [1.0, 2.0, 3.0]
 x = [1, 1, 1]
 
 n = 10
 
 gauss(A, b, x, n)
-# print(solve(A, b))
+print('Solution: ', solve(A, b))
 
-# %%
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
@@ -50,5 +42,4 @@ for i in range(n - 1):
             [vec_y[i], vec_y[i + 1]],
             zs=[vec_z[i], vec_z[i + 1]])
 
-
-# %%
+fig.savefig('plot.svg')
