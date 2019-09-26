32da9967fb867701609288573e9f88f16ffd0c5c
Stavros Filosidis <stavrosfil@gmail.com>
2019-09-26 19:15:45 +0300

    Improve visibility and add dots for answers

--- gauss.py
+++ gauss.py
@@ -30,11 +30,12 @@ A = np.array([[4.0, -2.0, 1.0],
 b = [1.0, 2.0, 3.0]
 x = [0, 0, 0]
 
+# Insert the guesses into the start of the anwers.
 vec_x.insert(x[0], 0)
 vec_y.insert(x[1], 0)
 vec_z.insert(x[2], 0)
 
-n = 5
+n = 4
 
 gauss(A, b, x, n)
 solution = solve(A, b)
@@ -47,22 +48,6 @@ ax = fig.add_subplot(111, projection='3d')
 ax.set_xlabel('X', fontsize='large', fontweight='bold', labelpad=12)
 ax.set_ylabel('Y', fontsize='large', fontweight='bold', labelpad=12)
 ax.set_zlabel('Z', fontsize='large', fontweight='bold', labelpad=12)
-
-# a plane is a*x+b*y+c*z+d=0
-# [a,b,c] is the normal.
-normal = A[0]
-d = -1
-
-# create x,y
-plane_space_x = np.arange(min(vec_x) - 0.2, max(vec_x) + 0.2, .1)
-plane_space_y = np.arange(min(vec_y) - 0.2, max(vec_y) + 0.2, .1)
-xx, yy = np.meshgrid(plane_space_x, plane_space_y)
-
-# calculate corresponding z
-z = (-normal[0] * xx - normal[1] * yy - d) * 1. / normal[2]
-
-# plot the surface
-ax.plot_surface(xx, yy, z, alpha=0.5)
 
 for i in range(n):
 
@@ -84,9 +69,26 @@ for i in range(n):
             zs=[vec_z[i], vec_z[i + 1]],
             linewidth = 3 / (i + 1))
 
-ax.scatter(x[0], x[1], x[2], c='r', marker='o')
-# ax.scatter(solution[0], solution[1], solution[2], c='g', marker='o')
+# Show a red circle for the first guess, and a green circle for the answer.
+ax.scatter(x[0], x[1], x[2], c='red', marker='o')
+ax.scatter(vec_x[-1], vec_y[-1], vec_z[-1], c='yellow', marker='o')
+ax.scatter(solution[0], solution[1], solution[2], c='green', marker='o')
 
+# a plane is a*x+b*y+c*z+d=0
+# [a,b,c] is the normal.
+normal = A[0]
+d = -1
+
+# create x,y
+plane_space_x = np.arange(min(vec_x) - 0.2, max(vec_x) + 0.2, .1)
+plane_space_y = np.arange(min(vec_y) - 0.2, max(vec_y) + 0.2, .1)
+xx, yy = np.meshgrid(plane_space_x, plane_space_y)
+
+# calculate corresponding z
+z = (-normal[0] * xx - normal[1] * yy - d) * 1. / normal[2]
+
+# plot the surface
+ax.plot_surface(xx, yy, z, alpha=0.2, color='grey')
 
 # rotate the axes and update
 # for angle in range(0, 360):
